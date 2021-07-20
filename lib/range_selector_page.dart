faabfe68ce717ecffc51ceba2015a471dd94fa74
S0han <43890209+S0han@users.noreply.github.com>
2021-07-19 23:56:26 -0400

    Add files via upload

---
+++ lib/range_selector_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/widgets.dart';
+
+class RangeSelectorPage extends StatefulWidget {
+  RangeSelectorPage({Key? key}) : super(key: key);
+
+  @override
+  _RangeSelectorPageState createState() => _RangeSelectorPageState();
+}
+
+class _RangeSelectorPageState extends State<RangeSelectorPage> {
+  final formKey = GlobalKey<FormState>();
+  int _min = 0;
+  int _max = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select Range'),
+      ),
+      body: Form(
+        key: formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RangeSelectorTextFormField(
+                labelText: 'Minimum',
+                intValueSetter: (value) => _min = value,
+              ),
+              SizedBox(height: 12),
+              RangeSelectorTextFormField(
+                labelText: 'Maximum',
+                intValueSetter: (value) => _max = value,
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_forward),
+        onPressed: () {
+          if (formKey.currentState?.validate() == true) {
+            formKey.currentState?.save();
+          }
+          //TODO: Navigate to the generator page
+        },
+      ),
+    );
+  }
+}
+
+class RangeSelectorTextFormField extends StatelessWidget {
+  const RangeSelectorTextFormField({
+    Key? key,
+    required this.labelText,
+    required this.intValueSetter,
+  }) : super(key: key);
+
+  final String labelText;
+  final void Function(int value) intValueSetter;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: labelText,
+      ),
+      keyboardType: TextInputType.numberWithOptions(
+        decimal: false,
+        signed: true,
+      ),
+      validator: (value) {
+        if (value == null || int.tryParse(value) == null) {
+          return 'Must be an integer';
+        } else {
+          return null;
+        }
+      },
+      onSaved: (newValue) => intValueSetter(
+        int.parse(newValue ?? ''),
+      ),
+    );
+  }
+}
